e5266472f56294d50fced17ddea3a66ee768d227
animesh65432 <kiranduttta234@gmail.com>
2025-02-04 19:31:27 +0530

    add docker file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // Alias for your `src` directory
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react") || id.includes("react-dom")) return "vendor-react";
+            if (id.includes("lodash")) return "vendor-lodash";
+            return "vendor"; // Move third-party libraries to a separate chunk
+          }
+        },
+      },
     },
+    chunkSizeWarningLimit: 1000, // Optional: Increase chunk warning limit
   },
 });
+
